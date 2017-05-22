11f83715163c40ef8cce97bba3f628c32a147114
Victor Alessander <alessander@protonmail.com>
2017-05-22 14:14:01 +0000

    Added functionalities

--- main.rb
+++ main.rb
@@ -6,8 +6,8 @@ require 'telegram/bot'
 token = '307623077:AAFGiQtnFsb5kVvnKtzDedSUOUpEDVTVfnI'
 
 
-def extract
-	link = "https://pt.stackoverflow.com/questions/tagged/python"
+def extract(tag)
+	link = "https://pt.stackoverflow.com/questions/tagged/#{tag}"
 	response = HTTParty.get(link)
 	content = Nokogiri::HTML(response)
 
@@ -23,6 +23,14 @@ def bot(token)
 			case message.text
 			when '/stackoverflow'
 				bot.api.send_message(chat_id: message.chat.id, text: extract)
+			when '/stackoverflow php'
+				bot.api.send_message(chat_id: message.chat.id, text: extract('php'))
+			when '/stackoverflow javascript'
+				bot.api.send_message(chat_id: message.chat.id, text: extract('javascript'))
+			when '/stackoverflow c#'
+				bot.api.send_message(chat_id: message.chat.id, text: extract('c#'))
+			when '/stackoverflow python'
+				bot.api.send_message(chat_id: message.chat.id, text: extract('python'))
 			end
 		end
 	end
